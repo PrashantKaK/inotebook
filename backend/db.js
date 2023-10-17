bd023f82b554a4e06e3d2126a76b559d9273895e
PrashantKaK <kakprashant@gmail.com>
2023-10-17 14:45:57 +0530

    Added Validator

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const mongoURI = "mongodb://0.0.0.0:27017/"; // Specify the database name in the URI
+const mongoURI = "mongodb://0.0.0.0:27017/iNoteBook"; // Specify the database name in the URI
 
 const connectToMongo = async () => {
   try {
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,15 +1,31 @@
 import express from "express";
 const router = express.Router();
-import User from "../models/User.js"
-
+import User from "../models/User.js";
+import { body, validationResult } from "express-validator";
+// import { ExpressValidator } from "express-validator";
 
 //create a User using: POST "/api/auth/". Doesn't require Auth
 
-router.post('/', (req, res) => {
-    console.log(req.body);
-    const user = User(req.body);
-    user.save()
-    res.send(req.body)
+router.post(
+  "/",
+  //express-validator used here
+  [
+    body("email").isEmail(),
+    body("name").isLength({ min: 3 }),
+    body("password").isLength({ min: 5 }),
+  ],
+  (req, res) => {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(400).json({ result: result.array() });
+    }//if there is no error then we send the response to the database
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    }).then(User => res.json(User))
+    .catch(err=>res.json({error: "Email already exist"}))
     
-});
+  }
+);
 export default router;
